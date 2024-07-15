90b70a16dae039935a7fd979f229206aec0c4c81
facelessDev <luyosamson@gmail.com>
2024-07-15 20:23:38 +0300

    modified  routes

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
+const express=require('express');
+const app=express();
 const path = require('path');
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT=process.env.PORT || 3500;
+
+app.get('/',(req,res)=>{
+    res.send("HELLO WORLD!");
+});
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
