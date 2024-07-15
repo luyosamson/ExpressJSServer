528649547eca25ec045490c3015cfe1281cfd8be
facelessDev <luyosamson@gmail.com>
2024-07-16 00:16:07 +0300

    New.html page route

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const path = require('path');
 const PORT=process.env.PORT || 3500;
 
 app.get('/',(req,res)=>{
-    res.send("HELLO WORLD!");
+    // res.sendFile('./views/index.html',{root:__dirname});
+    res.sendFile('./views/index.html',{root:__dirname});
+});
+
+app.get('/new-page.html',(req,res)=>{
+    // res.sendFile('./views/index.html',{root:__dirname});
+    res.sendFile(path.join(__dirname,'views','new-page.html'));
 });
 
 
