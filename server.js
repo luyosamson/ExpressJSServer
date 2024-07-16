0b2fac1c8b7f42cf8ed6ca0ec7eef80c1554ce5f
facelessDev <luyosamson@gmail.com>
2024-07-16 11:36:47 +0300

    chained routes

--- server.js
+++ server.js
@@ -19,12 +19,42 @@ app.get('/old-page(.html)?',(req,res)=>{
     res.redirect(301,'/new-page.html');
 });
 
+
+//Route Handlers
+app.get('/hello(.html)?',(req,res,next)=>{
+    console.log("Attempted to load Hello.html");
+    next()
+ },(req,res)=>{
+     res.send("Hello world!!!!")
+
+ })
+
+ //Chaining route handlers
+ const one=(req,res,next)=>{
+    console.log("ONE");
+    next()
+ }
+ const two=(req,res,next)=>{
+    console.log("TWO");
+    next()
+ }
+ 
+ const three=(reg,res)=>{
+    console.log("Three")
+    res.send("FINISHED")
+ }
+
+ app.get('/chain(.html)?',[one,two,three]);
+
+
+
 app.get('/*',(req,res)=>{
     res.status(404).sendFile(path.join(__dirname,'views','404.html'));//302 by default
 
 })
 
-//Route Handlers
+
+
 
 
 
