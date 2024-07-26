179632e7651f92a8c123dceb20a40d06190a3ac5
facelessDev <luyosamson@gmail.com>
2024-07-27 00:25:41 +0300

    Default unmatched routes

--- server.js
+++ server.js
@@ -82,10 +82,18 @@ app.get('/hello(.html)?',(req,res,next)=>{
 
 
 
-app.get('/*',(req,res)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'));//302 by default
-
-})
+app.all('*',(req,res)=>{
+    res.status(404);
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname,'views','404.html'));//302 by default
+    }
+    else if(req.accepts('json')){
+        res.json({error:"404 Not Found"});
+    }
+    else{
+        res.type('txt').send("404 Not Found");
+    }
+});
 
 app.use(errorHandler);
  
